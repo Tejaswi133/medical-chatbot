ed5991900c03f9df8ba5bf8e68cdc08ddaf81ff2
Tejaswi133-github <tejaswireddy912@gmail.com>
2025-07-26 13:16:29 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Tejaswi',
+    author_email= 'tejaswireddy912@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
